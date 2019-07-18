31b1e9c12278fe62f17ce516c7af5ac5de5a943d
djseong <dseong01@college.harvard.edu>
2019-07-17 20:08:05 -0400

    build folder

---
+++ build/precache-manifest.e064dffa117dcb9dc362433efdaeae15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b88dba1c807d5da97497226fc2b3c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0768c53495e2db7d6d1",
+    "url": "/static/css/main.4213f3d3.chunk.css"
+  },
+  {
+    "revision": "ca7d490ae6615af9ac7b",
+    "url": "/static/js/2.72840c1c.chunk.js"
+  },
+  {
+    "revision": "a0768c53495e2db7d6d1",
+    "url": "/static/js/main.57364d61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
